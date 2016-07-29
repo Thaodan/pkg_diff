19be26dcc4703c089abb1f308c67db4d6dd3b36a
Björn Bidar <theodorstormgrade@gmail.com>
2016-07-29 15:02:07 +0200

    updated (source) paths

--- src/pkg_diff.in.sh
+++ src/pkg_diff.in.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 appname=${0##*/}
 ver=@VER@
-source @libdir@/debug
 source @libdir@/parse_options
 source @libdir@/modules/config
 source @libdir@/modules/cache
 source @libdir@/modules/source
-source @libdir@/diff
+source @libdir@/modules/diff
 clean_up
